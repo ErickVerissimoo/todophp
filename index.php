0421b8a82be631ae0543081be1d20dcfee3f99be
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2025-02-19 21:16:51 -0300

    api mais limpa

--- index.php
+++ index.php
@@ -10,26 +10,24 @@ use Erick\Todo\mapper\UserMapper;
 use Erick\Todo\service\TaskService;
 use Erick\Todo\service\UserService;
 use Flight;
+
 $medoo = include 'src/utils/DatabaseConfig.php';
 $user= new UserService(new UserMapper($medoo));
 $task = new TaskService(new TaskMapper($medoo));
 
-Flight::route('GET /task/@id', function (int $id) use ($task) {
- $t = $task->get(id:$id );
- Flight::json($t, 200);
-});
+Flight::route('GET /task/@id',  fn (int $id) =>
+ Flight::json($task->get(id:$id ), 200));
+
 Flight::route('POST /task', function () use ($task) {
 $body = Flight::request()->data->getData();
 $task ->create(new Task($body));
 Flight::halt(201);
 });
-Flight::route('GET /task', function () use ($task) {
-    $tasks = [];
-foreach( $task->getAll() as $task ) {
-    array_push($tasks, $task);
-}  
-   Flight::json($tasks,200);
-});
+
+
+Flight::route('GET /task', fn()=>
+   Flight::json(iterator_to_array($task->getAll()),200)
+);
 
 Flight::route('PUT /task', function () use ($task) {
     $string =['message'=> 'teste'];
